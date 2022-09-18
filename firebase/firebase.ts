0e438bc13abc31f1af8cbda03c515d57e078e973
Ian Benton <bentonian1@gmail.com>
2022-09-18 11:38:57 -0700

    update

--- firebase/firebase.ts
+++ firebase/firebase.ts
@@ -3,11 +3,11 @@ import { getAuth } from 'firebase/auth';
 import { getDatabase } from 'firebase/database';
 
 const firebaseConfig = {
-  apiKey: 'AIzaSyA_HmYbBMgtvCYzTjhWfi_7pfaIvqpDRi0',
-  authDomain: 'hasura-firebase-4bbf4.firebaseapp.com',
-  projectId: 'hasura-firebase-4bbf4',
-  databaseUrl: 'https://hasura-firebase-4bbf4-default-rtdb.firebaseio.com',
-  storageBucket: 'hasura-firebase-4bbf4.appspot.com',
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  databaseUrl: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
   messagingSenderId: '519289364948',
   appId: '1:519289364948:web:bad7d10a86d99385b67641'
 };
--- next.config.js
+++ next.config.js
@@ -1,7 +1,13 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
+   env: {
+    NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY: process.env.NEXT_PUBLIC_FIREBASE_PUBLIC_API_KEY,
+    NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+    NEXT_PUBLIC_FIREBASE_DATABASE_URL: process.env.NEXT_PUBLIC_FIREBASE_DATABASE_URL,
+    NEXT_PUBLIC_FIREBASE_PROJECT_ID: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+    NEXT_PUBLIC_FIREBASE_STORAGE: process.env.NEXT_PUBLIC_FIREBASE_STORAGE,
+  }
 }
 
 module.exports = nextConfig
